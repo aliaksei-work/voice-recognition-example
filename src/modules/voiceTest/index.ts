35f4969ef8e441d673e47dede75aa807bbc3fee4
Aliaksei Tsiareshka <aliaksei.tsiareshka.work@gmail.com>
2025-06-18 21:28:51 +0100

    cleanup

--- src/modules/voiceTest/index.ts
+++ src/modules/voiceTest/index.ts
@@ -1,8 +1,1 @@
-export { default as VoiceTest } from './VoiceTestFuncComp';
-export { VoiceHeader } from './components/VoiceHeader';
-export { VoiceStatus } from './components/VoiceStatus';
-export { VoiceControls } from './components/VoiceControls';
-export { LanguageSelector } from './components/LanguageSelector';
-export { useVoiceRecognition } from './hooks/useVoiceRecognition';
-export { useListeningState } from './hooks/useListeningState';
-export type { VoiceState, Language } from './hooks/useVoiceRecognition'; 
+export {default as VoiceTest} from './VoiceTest';
